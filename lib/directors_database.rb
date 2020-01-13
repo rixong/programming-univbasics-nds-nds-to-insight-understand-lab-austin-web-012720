16712e4b1d12cc802ea64ac0bc14b2802838ed5c
Rick Glascock <rixong@gmail.com>
2020-01-13 13:39:32 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -4,7 +4,4 @@ def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
 end
-
-def print_first_directors_movie_titles(nds)
-
-end
+x
