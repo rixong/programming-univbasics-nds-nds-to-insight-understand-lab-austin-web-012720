f6f9855a2fbc1b930c58c3833ed33e11369d2886
Rick Glascock <rixong@gmail.com>
2020-01-13 13:38:52 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -3,14 +3,8 @@ require 'yaml'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-  pp rot13
 end
 
 def print_first_directors_movie_titles(nds)
-  count = 0
-  cur_dir_movies = nds[0][:movies]
-  while count < cur_dir_movies.length do
-    puts [count]
-    count +=1
-  end
+
 end
