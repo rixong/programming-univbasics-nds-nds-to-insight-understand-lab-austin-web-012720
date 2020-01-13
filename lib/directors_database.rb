9b7865e045f5f7da941605fb2d7fb7cee18a612a
Rick Glascock <rixong@gmail.com>
2020-01-13 13:31:18 +0000

    Automatically backed up by Learn

--- lib/directors_database.rb
+++ lib/directors_database.rb
@@ -3,8 +3,5 @@ require 'yaml'
 def directors_database
 	rot13 = -> (s) { s.tr('A-Za-z', 'N-ZA-Mn-za-m') }
   @_db ||= YAML.load(rot13.call(File.open("directors_db").read.to_s))
-end
-
-def pretty_print_nds(nds)
-  pp nds
+  pp rot13
 end
